c9ee329f2af23c72b1f7e350352eeb6cae54a9b2
Bright-Mensah <mensahokobright@gmail.com>
2024-12-13 11:55:18 +0000

    testing the api endpoint

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,8 @@ Route::get('/todos', [TodoController::class, 'index']); // List with filters & s
 Route::post('/todos', [TodoController::class, 'store']); // Create
 Route::put('/todos/{id}', [TodoController::class, 'update']); // Update
 Route::delete('/todos/{id}', [TodoController::class, 'destroy']); // Delete
+
+Route::get("/test", function (Request $request) {
+
+    return response()->json(["data" => "sadhasdhsa", "mob", "qwieowei"]);
+});
